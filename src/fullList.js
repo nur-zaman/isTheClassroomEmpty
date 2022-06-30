fb4a6f1b62c8d78d4d3815ef5a961a4d1c3efb1e
NUR UZ ZAMAN <48312903+nur-zaman@users.noreply.github.com>
2022-06-30 23:32:43 +0600

    added Full List
    Added Complete list of empty classrooms in current moment

---
+++ src/fullList.js
@@ -0,0 +1,43 @@
+const semester = 'summer2022'
+const getDataFromLocalStorage = (key)=> {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+data = getDataFromLocalStorage(semester)
+
+const date = moment().format("YYYY-MM-DD")
+
+const day = moment().format("dddd");  
+const now = moment().toString();
+
+// const now = 'Tue Jun 30 2022 05:05:34 pm'
+// const day = 'Tuesday'
+
+
+const addData=(tableData)=>{
+    let newRow = document.createElement("tr");
+    let newCell = document.createElement("td");
+    newCell.classList.add("border", "border-slate-600");
+    newCell.innerHTML = tableData;
+    newRow.append(newCell);
+    document.getElementById("rows").appendChild(newRow);
+}
+
+let classRoomList = []
+const showFullList = ()=>{
+    data.forEach(i => {
+        if(!(i.day==day && moment(now).isBetween(`${date} ${i.start}`, `${date} ${i.end}`))){
+            classRoomList.push(i.room)
+            
+        }
+    });
+
+    classRoomList.sort().reverse()
+for (const i of classRoomList ) {
+    addData(i)
+}
+
+    document.querySelector('#update').innerHTML = moment()
+}
+
+showFullList()
