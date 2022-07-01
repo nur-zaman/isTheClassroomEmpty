3a2ae3823e497fc268a2b81ed9047669973dd822
NUR UZ ZAMAN <48312903+nur-zaman@users.noreply.github.com>
2022-07-01 15:45:40 +0600

    added pwa functionalities

--- fullList.js
+++ fullList.js
@@ -1,43 +1,47 @@
-const semester = 'summer2022'
-const getDataFromLocalStorage = (key)=> {
-    return JSON.parse(localStorage.getItem(key));
-  }
+const semester = "summer2022";
+const getDataFromLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
 
-data = getDataFromLocalStorage(semester)
+data = getDataFromLocalStorage(semester);
 
-const date = moment().format("YYYY-MM-DD")
+const date = moment().format("YYYY-MM-DD");
 
-const day = moment().format("dddd");  
+const day = moment().format("dddd");
 const now = moment().toString();
 
 // const now = 'Tue Jun 30 2022 05:05:34 pm'
 // const day = 'Tuesday'
 
+const addData = (tableData) => {
+  let newRow = document.createElement("tr");
+  let newCell = document.createElement("td");
+  newCell.classList.add("border", "border-slate-600");
+  newCell.innerHTML = tableData;
+  newRow.append(newCell);
+  document.getElementById("rows").appendChild(newRow);
+};
 
-const addData=(tableData)=>{
-    let newRow = document.createElement("tr");
-    let newCell = document.createElement("td");
-    newCell.classList.add("border", "border-slate-600");
-    newCell.innerHTML = tableData;
-    newRow.append(newCell);
-    document.getElementById("rows").appendChild(newRow);
-}
+let classRoomList = [];
+const showFullList = () => {
+  data.forEach((i) => {
+    if (
+      !(
+        i.day == day &&
+        moment(now).isBetween(`${date} ${i.start}`, `${date} ${i.end}`)
+      )
+    ) {
+      classRoomList.push(i.room);
+    }
+  });
 
-let classRoomList = []
-const showFullList = ()=>{
-    data.forEach(i => {
-        if(!(i.day==day && moment(now).isBetween(`${date} ${i.start}`, `${date} ${i.end}`))){
-            classRoomList.push(i.room)
-            
-        }
-    });
+  classRoomList.sort().reverse();
+  classRoomList = [...new Set(classRoomList)]
+  for (const i of classRoomList) {
+    addData(i);
+  }
 
-    classRoomList.sort().reverse()
-for (const i of classRoomList ) {
-    addData(i)
-}
+  document.querySelector("#update").innerHTML = moment();
+};
 
-    document.querySelector('#update').innerHTML = moment()
-}
-
-showFullList()
+showFullList();
